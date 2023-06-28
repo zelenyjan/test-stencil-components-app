91d73343aa676026d0cf8d4952c84a60f03a57bc
Jan Zeleny <zelenja8@gmail.com>
2023-06-28 09:29:54 +0200

    Improve design :see_no_evil:

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,22 @@ const setToGovDefault = (e) => {
 </script>
 
 <template>
-  <h1>{{value}}</h1>
-
-  <GreenButton :label="value" @click="setToDefault"></GreenButton>
-  <GreenInput v-model="value"></GreenInput>
-
+  <div style="padding: 20px; background: #d7a0a0">
+    <h1>{{ value }}</h1>
+    <GreenButton :label="value" @click="setToDefault"></GreenButton>
+    <GreenInput v-model="value"></GreenInput>
+  </div>
   <hr>
+  <hr>
+  <hr>
+  <hr>
+  <div style="padding: 20px; background: #aab3f1">
+    <div>GOV komponenty</div>
 
-  <h1>{{govValue}}</h1>
-  <GovButton size="xl" variant="primary" type="solid" native-type="button" wcag-label="Klikni na mě a uvidíš zázrak!" @click="setToGovDefault">{{govValue}}</GovButton>
-  <GovFormInput v-model="govValue" variant="primary"></GovFormInput>
+    <h1>{{ govValue }}</h1>
+    <GovButton size="xl" variant="primary" type="solid" native-type="button" wcag-label="Klikni na mě a uvidíš zázrak!"
+               @click="setToGovDefault">{{ govValue }}
+    </GovButton>
+    <GovFormInput v-model="govValue" variant="primary"></GovFormInput>
+  </div>
 </template>
